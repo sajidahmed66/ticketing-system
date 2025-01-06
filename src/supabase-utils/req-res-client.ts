8b9d823a8cb061d74273d1735e9c617c3d8544f8
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2025-01-06 20:30:19 +0600

    supabase clients crated

--- src/supabase-utils/req-res-client.ts
+++ src/supabase-utils/req-res-client.ts
@@ -1,12 +1,12 @@
-import {createServerClient} from "@supabase/ssr";
+import {createServerClient, serializeCookieHeader} from "@supabase/ssr";
 import {NextResponse} from "next/server";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-export const getSupabaseReqResClient = ({request}) => {
+export const getSupabaseReqResClient = ({request, response: responseInput}) => {
 
-    const response = {
-        value: NextResponse.next({request: request})
+    let response = {
+        value: responseInput ?? NextResponse.next({request: request})
     };
 
     const supabase = createServerClient(
@@ -15,18 +15,35 @@ export const getSupabaseReqResClient = ({request}) => {
         {
             cookies: {
                 getAll() {
-                    return request.cookies.getAll();
+                    if ("getAll" in request.cookies) {
+                        return request.cookies.getAll();
+                    } else {
+                        return Object.keys(request.cookies).map((name) => ({
+                            name,
+                            value: request.cookies[name]
+                        }))
+                    }
                 },
                 setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({name, value, options}) => {
-                        request.cookies.set(name, value);
-                    });
-                    response.value = NextResponse.next({
-                        request,
-                    });
-                    cookiesToSet.forEach(({name, value, options}) => {
-                        response.value.cookies.set(name, value, options);
-                    });
+                    if ("getAll" in request.cookies) {
+                        cookiesToSet.forEach(({name, value, options}) => {
+                            request.cookies.set(name, value);
+                        });
+                        response.value = NextResponse.next({
+                            request,
+                        });
+                        cookiesToSet.forEach(({name, value, options}) => {
+                            response.value.cookies.set(name, value, options);
+                        });
+                    } else {
+                        //page router /not middleware
+                        responseInput.setHeader(
+                            "Set-Cookie",
+                            cookiesToSet.map(({name, value, options}) =>
+                                serializeCookieHeader(name, value, options)
+                            )
+                        )
+                    }
                 },
             },
         }
